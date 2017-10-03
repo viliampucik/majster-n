0ab351df8676d035878ba5c5af7adfc575ee8a2f
Viliam Pucik <viliam.pucik@protonmail.com>
2017-10-03 02:19:23 +0200

    Prva verzia

---
+++ README.md
@@ -0,0 +1,38 @@
+Majter N! 1.0
+======
+
+Nepi Jano! je doplnok pre webove prehliadace Google Chrome a Mozilla Firefox umoznujuci bezplatne citanie platenych clankov na [dennikn.sk](https://dennikn.sk). Staci si dane rozsirenie jednoducho nainstalovat z oficialnych stranok danych prehliadacov. Nasledne po otvoreni akehokolvek plateneho clanku sa automaticky, v priebehu par sekund nacita jeho plna verzia.
+
+## Instalacia
+
+### Google Chrome
+
+* Rozsirenie pre Chrome sa coskoro bude dat instalovat z oficialnej stranky Chrome Store.
+* Pripadne sa da nainstalovat manualne [stiahnutim z tejto stranky](https://github.com/viliampucik/majster-n/raw/master/releases/majster-n-1.0.crx),
+* otvorenim zalozky doplnkov kliknutim na menu *-> Nastavenia -> Rozsirenia*
+* a naslednym pretiahnutim stiahnuteho *.crx* suboru do spominanej zalozky.
+
+### Mozilla Firefox
+
+* [Doplnok pre Firefox sa instaluje z oficialnej stranky Mozilla doplnkov](https://addons.mozilla.org/sk/firefox/addon/majster-n/).
+
+## Zoznam zmien
+
+#### 1.0
+
+* Prva verzia
+
+## Licencia
+
+GPL a MIT
+
+## O autoroch
+
+##### Autor
+* [Viliam Pucik](https://github.com/viliampucik)
+
+##### Prispievatelia
+* [sveco86](https://github.com/sveco86)
+* [timelordx](https://github.com/timelordx)
+* Jozef Giusseppe
+* and dalsi
---
+++ webextension/article.js
@@ -0,0 +1,97 @@
+/**
+ * Some utils
+ */
+var utils = {};
+
+utils.whitelist = ['#text', 'A', 'ABBR', 'ADDRESS', 'AREA', 'ARTICLE', 'ASIDE', 'AUDIO', 'B', 'BDI', 'BDO', 'BLOCKQUOTE', 'BR', 'BUTTON', 'CAPTION', 'CITE', 'CODE', 'COL', 'COLGROUP', 'DATA', 'DATALIST', 'DD', 'DEL', 'DFN', 'DIV', 'DL', 'DT', 'EM', 'EMBED', 'FIELDSET', 'FIGCAPTION', 'FIGURE', 'FOOTER', 'FORM', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'HEADER', 'HR', 'I', 'IFRAME', 'IMG', 'INPUT', 'INS', 'KBD', 'KEYGEN', 'LABEL', 'LEGEND', 'LI', 'MAIN', 'MAP', 'MARK', 'METER', 'NAV', 'OBJECT', 'OL', 'OPTGROUP', 'OPTION', 'OUTPUT', 'P', 'PARAM', 'PRE', 'PROGRESS', 'Q', 'RB', 'RP', 'RT', 'RTC', 'RUBY', 'S', 'SAMP', 'SECTION', 'SELECT', 'SMALL', 'SOURCE', 'SPAN', 'STRONG', 'SUB', 'SUP', 'TABLE', 'TBODY', 'TD', 'TEXTAREA', 'TFOOT', 'TH', 'THEAD', 'TIME', 'TR', 'TRACK', 'U', 'UL', 'VAR', 'VIDEO', 'WBR'];
+
+/**
+ * Get article ID from URL
+ */
+utils.articleId = function() {
+	var match = document.location.href.match(/^https:\/\/dennikn\.sk\/(\d+).*/);
+	return (match !== null && match.length >= 2) ? match[1] : null;
+};
+
+/**
+ * Detect paid article
+ */
+utils.isPaidArticle = function() {
+	return document.querySelector('.e_lockhard');
+};
+
+/**
+ * Copy only allowed HTML elements and their styles from the remote article
+ */
+utils.sanitizeContent = function(root, node) {
+	for(var i = 0; i < node.childNodes.length; i++ ) {
+		var child = node.childNodes[i];
+
+		if (utils.whitelist.indexOf(child.nodeName) >= 0) {
+			var element;
+
+			if (child.nodeName == '#text') {
+				element = document.createTextNode(child.textContent);
+			}
+			else {
+				element = document.createElement(child.nodeName);
+				if (child.id.length > 0) {
+					element.id = child.id;
+				}
+				if (child.className.length > 0) {
+					element.className = child.className;
+				}
+				if (child.style.cssText.length > 0) {
+					element.style.cssText = child.style.cssText;
+				}
+
+				if (child.nodeName == 'A') {
+					element.href = child.href;
+				}
+				else if (child.nodeName == 'IFRAME') {
+					element.src = child.src;
+				}
+				else if (child.nodeName == 'IMG') {
+					element.src = child.src;
+					if (child.alt.length > 0) {
+						element.alt = child.alt;
+					}
+				}
+
+				if (child.childNodes.length > 0) {
+					utils.sanitizeContent(element, child);
+				}
+			}
+
+			root.appendChild(element);
+		}
+	}
+};
+
+/**
+ * Get mobile version of the article
+ */
+utils.getArticle = function(url) {
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	request.onload = function() {
+		if (request.status == 200) {
+			try {
+				var json = JSON.parse(request.responseText);
+				var doc = (new DOMParser()).parseFromString(json.articles[0].content, 'text/html');
+				var html = document.querySelector('.d-content');
+				html.innerHTML = '';
+				utils.sanitizeContent(html, doc.querySelector('body'));
+			}
+			catch (e) {
+				console.log(`majster-n: ${e}`);
+			}
+		}
+	};
+	request.send();
+};
+
+var articleId = utils.articleId();
+if (articleId && utils.isPaidArticle()) {
+	utils.getArticle('https://dennikn.sk/api/reader?id=' + articleId);
+}
